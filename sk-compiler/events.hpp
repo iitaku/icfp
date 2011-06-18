a8388f2bb91e37c6bdff86c323a57f6fd379a541
Takashi Nakamura <nakamura@fixstars.com>
2011-06-19 05:14:18 +0900

    event

---
+++ sk-compiler/events.hpp
@@ -0,0 +1,37 @@
+#ifndef EVENT_HPP
+#define EVENT_HPP
+
+#include <vector>
+
+namespace copy_kawaii {
+
+struct Event {
+    enum event_code {
+        PROP_DEAD,
+        TYPE_ERROR,
+        OPP_PUT_ZOMBIE
+    } code;
+
+    union {
+        int slot;
+
+        struct {
+            
+        } xyzzy;
+
+        float xyzzy2;
+    } u;
+
+    static Event dead(enum event_code code, int slot) {
+        Event e;
+        e.code = PROP_DEAD;
+        e.u.slot = slot;
+        return e;
+    }
+};
+
+typedef std::vector<Event> event_list_t;
+
+}
+
+#endif
